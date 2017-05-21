ddaae38c46e7179e472405ae7133f1dd066dffee
yu2003w <yu2003w@hotmail.com>
2017-05-22 01:24:00 +0800

    moved scripts into another diretory

--- installdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker exec -u root web-mysql /bin/sh -c 'mysql -u root -p"mysql" < /root/mysql/schema/install_infocenter'
--- startmysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf /home/jared/bench/mysql/schema/
-cp -rf /home/repo/ustudy/schema/ /home/jared/bench/mysql/
-
-docker run --rm -it --name web-mysql -v /home/jared/bench/mysql/data:/var/lib/mysql \
-    -v /home/jared/bench/mysql/schema/:/root/mysql/schema/ -p 13306:3306 -e MYSQL_ROOT_PASSWORD=mysql mysql:5.7
-
-# docker exec -u root web-mysql /bin/sh -c 'mysql -u root -p"mysql" < /root/mysql/schema/install_infocenter'
-
