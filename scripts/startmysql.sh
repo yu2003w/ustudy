9df9bf557b3d600bc580e6636707d2cc4fe873d6
yu2003w <yu2003w@hotmail.com>
2017-05-31 14:50:32 +0800

    Enable mysql logs when starting mysql container

--- scripts/startmysql.sh
+++ scripts/startmysql.sh
@@ -4,7 +4,7 @@ if [ $# != 1 ]; then
   exit
 fi
 WORK_DIR=$1
-echo "Working directory is " ${WORK_DIR}
+echo "Using ${WORK_DIR} as working directory"
 if [ ! -d ${WORK_DIR}/mysql/schema/ ]; then
   mkdir -p ${WORK_DIR}/mysql/schema/ 
   if [ $? != 0 ]; then
@@ -19,9 +19,25 @@ if [ ! -d ${WORK_DIR}/mysql/schema/ ]; then
   fi
 fi
 
+MYSQL_LOG_DIR=${WORK_DIR}/logs/mysql/
+if [ ! -d ${MYSQL_LOG_DIR} ]; then
+  mkdir -p ${MYSQL_LOG_DIR}
+  if [ $? != 0 ]; then
+    echo "Failed to create directory for mysql logs"
+    exit
+  else
+    echo "Create directory ${MYSQL_LOG_DIR} for mysql logs successfully"
+  fi
+fi
 
+# To specify log file name, use '--general_log_file gen.log' as needed
+# add more mysql logs
 docker run --rm -it --name web-mysql -v ${WORK_DIR}/mysql/data:/var/lib/mysql \
-    -v ${WORK_DIR}/mysql/schema/:/root/mysql/schema/ -p 13306:3306 -e MYSQL_ROOT_PASSWORD=mysql mysql:5.7
+    -v ${WORK_DIR}/mysql/schema/:/root/mysql/schema/ -v ${MYSQL_LOG_DIR}:/var/lib/mysql/logs/ \
+    -p 13306:3306 -e MYSQL_ROOT_PASSWORD=mysql -d mysql:5.7 \
+    --log-error="logs/mysqld.log"  \
+    --general_log=1 --general_log_file logs/gen.log \
+    --slow_query_log=1 --slow_query_log_file logs/slow.log
 
 # docker exec -u root web-mysql /bin/sh -c 'mysql -u root -p"mysql" < /root/mysql/schema/install_infocenter'
 
