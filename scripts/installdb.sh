dc75b144030ccb80beab78d2f9b88f0d06ba16a0
yu2003w <yu2003w@hotmail.com>
2017-06-04 09:58:22 +0800

    enhance scripts

--- scripts/installdb.sh
+++ scripts/installdb.sh
@@ -11,3 +11,11 @@ else
   echo "Install database for service infocenter failed"
 fi
 
+# databases for infocenter
+docker exec -u root web-mysql /bin/sh -c 'mysql -u root -p"mysql" < /root/mysql/schema/install_admin'
+if [ $? = 0 ]; then
+  echo "Install database for service admin successfully"
+else
+  echo "Install database for service admin failed"
+fi
+
