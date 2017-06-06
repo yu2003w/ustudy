5a32e5b7b8c210656cdae297b07d283223c3c1d1
yu2003w <yu2003w@hotmail.com>
2017-06-07 03:05:49 +0800

    Update applicationContext.xml and import when application started

--- adminboard/src/main/java/com/ustudy/admin/AdminBoard.java
+++ adminboard/src/main/java/com/ustudy/admin/AdminBoard.java
@@ -3,8 +3,10 @@ package com.ustudy.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@ImportResource({"classpath*:applicationContext.xml"})
 public class AdminBoard {
 
 	public static void main(String[] args) throws Exception {
