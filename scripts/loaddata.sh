6bc198bb31421ca96a8763750cd5e18910f47e8b
yu2003w <yu2003w@hotmail.com>
2017-06-14 01:49:06 +0800

    fixed servlet-mapping problem in pom.xml

--- scripts/loaddata.sh
+++ scripts/loaddata.sh
@@ -4,10 +4,10 @@
 # load data for testing purpose
 
 # load data for infocenter.student
-docker exec ustudy-dw sh -c 'mysql -uroot -p"mysql" < /root/mysql/schema/load_stu'
+docker exec ustudy-dw sh -c 'mysql -uroot -p"mysql" < /root/mysql/schema/load_data'
 if [ $? = 0 ]; then
-  echo "load data for infocenter.student successful"
+  echo "load data for testing successful"
 else
-  echo "load data for infocenter.student failed"
+  echo "load data for testing failed"
 fi
 
