feda398b91ff833d4d0a292d2864f35324588d2f
yu2003w <yu2003w@hotmail.com>
2017-06-14 01:55:41 +0800

    update code

--- dashboard/src/main/java/com/ustudy/dashboard/ServletInitializer.java
+++ dashboard/src/main/java/com/ustudy/dashboard/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(this.getClass());
+		return application.sources(Dashboard.class);
 	}
 
 }
