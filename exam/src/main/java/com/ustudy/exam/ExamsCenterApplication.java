9e63f8f6126043ac60b85c134fc7f703ec73955d
yu2003w <yu2003w@hotmail.com>
2017-09-04 15:56:58 +0800

    Initial check in for exam center

---
+++ exam/src/main/java/com/ustudy/exam/ExamsCenterApplication.java
@@ -0,0 +1,12 @@
+package com.ustudy.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsCenterApplication.class, args);
+	}
+}
