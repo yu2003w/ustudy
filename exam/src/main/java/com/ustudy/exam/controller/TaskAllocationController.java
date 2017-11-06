388cb7792ee66796e8c243666e76d9dbda2e7c19
dulei2085 <645744938@qq.com>
2017-11-06 22:29:51 +0800

    任务分配 - 获取试题结构接口

--- exam/src/main/java/com/ustudy/exam/controller/TaskAllocationController.java
+++ exam/src/main/java/com/ustudy/exam/controller/TaskAllocationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ustudy.exam.service.SetAnswersService;
+import com.ustudy.exam.service.TaskAllocationService;
 
 @RestController
 @RequestMapping(value = "/task/allocation")
@@ -24,7 +24,7 @@ public class TaskAllocationController {
 	private static final Logger logger = LogManager.getLogger(TaskAllocationController.class);
 
 	@Autowired
-	private SetAnswersService service;
+	private TaskAllocationService service;
 
 	@RequestMapping(value = "/questions/{egsId}", method = RequestMethod.GET)
 	public Map getQuestions(@PathVariable Long egsId, HttpServletRequest request, HttpServletResponse response) {
@@ -36,7 +36,27 @@ public class TaskAllocationController {
 
 		try {
 			result.put("success", true);
-			result.put("data", service.getQuesAnswer(egsId));
+			result.put("data", service.getQuestions(egsId));
+		} catch (Exception e) {
+			result.put("success", false);
+			result.put("message", e.getMessage());
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/questions/{examId}/{gradeId}/{subjectId}", method = RequestMethod.GET)
+	public Map getQuestions(@PathVariable Long examId, @PathVariable Long gradeId, @PathVariable Long subjectId, HttpServletRequest request, HttpServletResponse response) {
+
+		logger.debug("getQuestions().");
+		logger.debug("examId: " + examId + ",gradeId: " + gradeId + ",subjectId: " + subjectId);
+
+		Map result = new HashMap<>();
+
+		try {
+			result.put("success", true);
+			result.put("data", service.getQuestions(examId, gradeId, subjectId));
 		} catch (Exception e) {
 			result.put("success", false);
 			result.put("message", e.getMessage());
--- exam/src/main/java/com/ustudy/exam/service/impl/TaskAllocationServiceImpl.java
+++ exam/src/main/java/com/ustudy/exam/service/impl/TaskAllocationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ustudy.exam.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +13,8 @@ import com.ustudy.exam.dao.MultipleScoreSetDao;
 import com.ustudy.exam.dao.QuesAnswerDao;
 import com.ustudy.exam.dao.QuesAnswerDivDao;
 import com.ustudy.exam.dao.RefAnswerDao;
+import com.ustudy.exam.model.ExamSubject;
+import com.ustudy.exam.model.QuesAnswer;
 import com.ustudy.exam.service.TaskAllocationService;
 
 import net.sf.json.JSONArray;
@@ -38,13 +42,33 @@ public class TaskAllocationServiceImpl implements TaskAllocationService {
 
 	public JSONArray getQuestions(Long egsId) throws Exception {
 		logger.info("getQuestions -> egsId=" + egsId);
-		examSubjectDaoImpl.getExamSubjectById(egsId);
-		return null;
+		JSONArray array = new JSONArray();
+		List<QuesAnswer> quesAnswers = quesAnswerDaoImpl.getQuesAnswers(egsId);
+		for (QuesAnswer quesAnswer : quesAnswers) {
+			JSONObject object = new JSONObject();
+			object.put("id", quesAnswer.getId());
+			if(quesAnswer.getStartno() == quesAnswer.getEndno()){
+				object.put("questionName", "" + quesAnswer.getStartno());
+			}else {
+				object.put("questionName", quesAnswer.getStartno() + "-" + quesAnswer.getEndno());
+			}
+			object.put("type", quesAnswer.getType());
+			
+			array.add(object);
+		}
+		return array;
 	}
 	
 	public JSONArray getQuestions(Long examId, Long gradeId, Long subjectId) throws Exception {
 		logger.info("getQuestions -> examId=" + examId + ",gradeId=" + gradeId + ",subjectId=" + subjectId);
-		return null;
+		JSONArray array = new JSONArray();
+		List<ExamSubject> examSubjects = examSubjectDaoImpl.getExamSubjectByExamIdAndGradeIdAndSubjectId(examId, gradeId, subjectId);
+		if(null != examSubjects && examSubjects.size()>0){
+			Long egsId = examSubjects.get(0).getId();
+			array = getQuestions(egsId);
+		}
+		
+		return array;
 	}
 
 	public JSONObject getSchools(Long schoolId) throws Exception {
