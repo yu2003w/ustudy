509123ec56010235fb0100d84eac23c66f4e87d0
dulei2085 <645744938@qq.com>
2017-11-21 20:40:26 +0800

    update

--- exam/src/main/java/com/ustudy/exam/service/impl/StudentAnswerServiceImpl.java
+++ exam/src/main/java/com/ustudy/exam/service/impl/StudentAnswerServiceImpl.java
@@ -30,54 +30,61 @@ public class StudentAnswerServiceImpl implements StudentAnswerService {
 		
 		logger.debug("saveStudentsAnswers -> examId:" + examId + ", egsId:" + egsId + ", data:" + data);
 		
-		if (null != data.get("sipisModelList")) {
-			JSONArray studentPapers = data.getJSONArray("sipisModelList");
-			for (int i = 0; i < studentPapers.size(); i++) {
-				JSONObject object = studentPapers.getJSONObject(i);
-				StudentPaper paper = new StudentPaper();
-				paper.setExamId(examId);
-				paper.setEgsId(egsId);
-				if(null != object.get("ESID")){
-					paper.setStuExamNo(object.getString("ESID"));
+		try {
+			if (null != data.get("sipisModelList")) {
+				JSONArray studentPapers = data.getJSONArray("sipisModelList");
+				for (int i = 0; i < studentPapers.size(); i++) {
+					JSONObject object = studentPapers.getJSONObject(i);
+					StudentPaper paper = new StudentPaper();
+					paper.setExamId(examId);
+					paper.setEgsId(egsId);
+					if(null != object.get("ESID")){
+						paper.setStuExamNo(object.getString("ESID"));
+					}
+					if(null != object.get("BatchNum")){
+						paper.setBatchNum(object.getInt("BatchNum"));
+					}
+					if(null != object.get("StuAPPath")){
+						paper.setPaperImg(object.getString("StuAPPath"));
+					}
+					if(null != object.get("PaperStatus")){
+						paper.setPaperStatus(object.getString("PaperStatus"));
+					}
+					if(null != object.get("HistoryErrorStatusList")){
+						String errorStatus = object.getJSONArray("HistoryErrorStatusList").toString();
+						errorStatus = errorStatus.replace("[", "").replace("]", "");
+						paper.setErrorStatus(errorStatus);
+					}
+					paperDaoImpl.insertStudentPaper(paper);
+					
+					JSONArray studentScores = object.getJSONArray("Exam_Student_Score");
+					for (int j = 0; j < studentScores.size(); j++) {
+						JSONObject object_ = studentScores.getJSONObject(j);
+						
+						StudentObjectAnswer answer = new StudentObjectAnswer();
+						answer.setPaperid(paper.getId());
+						if(null != object_.get("questNum")){
+							answer.setQuesno(object_.getInt("questNum"));
+						}
+						if(null != object_.get("answerHas")){
+							answer.setAnswerHas(object_.getInt("answerHas"));
+						}
+						if(null != object_.get("stuObjectAnswer")){
+							answer.setAnswer(object_.getString("stuObjectAnswer"));
+						}
+						
+						answerDaoImpl.insertStudentObjectAnswer(answer);
+					}
 				}
-				if(null != object.get("BatchNum")){
-					paper.setBatchNum(object.getInt("BatchNum"));
-				}
-				if(null != object.get("StuAPPath")){
-					paper.setPaperImg(object.getString("StuAPPath"));
-				}
-				if(null != object.get("PaperStatus")){
-					paper.setPaperStatus(object.getString("PaperStatus"));
-				}
-				if(null != object.get("HistoryErrorStatusList")){
-					String errorStatus = object.getJSONArray("HistoryErrorStatusList").toString();
-					errorStatus = errorStatus.replace("[", "").replace("]", "");
-					paper.setErrorStatus(errorStatus);
-				}
-				paperDaoImpl.insertStudentPaper(paper);
 				
-				JSONArray studentScores = object.getJSONArray("Exam_Student_Score");
-				for (int j = 0; j < studentScores.size(); j++) {
-					JSONObject object_ = studentScores.getJSONObject(j);
-					
-					StudentObjectAnswer answer = new StudentObjectAnswer();
-					answer.setPaperid(paper.getId());
-					if(null != object_.get("questNum")){
-						answer.setQuesno(object_.getInt("questNum"));
-					}
-					if(null != object_.get("answerHas")){
-						answer.setAnswerHas(object_.getInt("answerHas"));
-					}
-					if(null != object_.get("stuObjectAnswer")){
-						answer.setAnswer(object_.getString("stuObjectAnswer"));
-					}
-					
-					answerDaoImpl.insertStudentObjectAnswer(answer);
-				}
+				return true;
+			}else {
+				return false;
 			}
-			
-			return true;
-		}else {
+		} catch (Exception e) {
+			logger.error("saveStudentsAnswers -> examId:" + examId + ", egsId:" + egsId + ", data:" + data);
+			logger.error("saveStudentsAnswers -> errorMsg:" + e.getMessage());
+			e.printStackTrace();
 			return false;
 		}
 	}
