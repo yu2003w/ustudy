751fc94ffe8d13da783d1e460abf9570e84cf7c9
leekiea <17413021@qq.com>
2018-04-09 23:14:16 +0800

    new feature: merge mark images into exam paper

--- exam/src/main/java/com/ustudy/exam/dao/ExamSubjectDao.java
+++ exam/src/main/java/com/ustudy/exam/dao/ExamSubjectDao.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.ustudy.exam.model.ExamSubject;
+import com.ustudy.exam.model.MarkImage;
 
 @Mapper
 public interface ExamSubjectDao {
@@ -37,6 +38,14 @@ public interface ExamSubjectDao {
 
 	void updateMarkSwitch(@Param("examId")Long examId, @Param("gradeId")Long gradeId, @Param("subjectId")Long subjectId, @Param("release")Boolean release);
 	
+	List<MarkImage> getDblMarkImgs(@Param("egsId")Long egsId);
+
+	void updateDblMarkImgs(@Param("quesId")Long quesId, @Param("paperId")Long paperId, @Param("qareaId")Long qareaId, @Param("markImg")String markImg);
+
+	List<MarkImage> getFinalMarkImgs(@Param("egsId")Long egsId);
+
+	void updateMarkImg(@Param("paperId")Long paperId, @Param("markImg")String markImg);
+
 	void isTaskDispatch(Long id);
     
 	void updateExamSubjectStatusById(@Param("egsId")Long egsId, @Param("release")Boolean release);
