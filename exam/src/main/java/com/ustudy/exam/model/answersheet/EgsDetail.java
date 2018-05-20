5ec9e804a5864831fe2decd6c319fa4a1f9a6555
weiyu <weiyu@asiainfo.com>
2018-05-20 11:29:06 +0800

    added question id for answer sheet meta information

--- exam/src/main/java/com/ustudy/exam/model/answersheet/EgsDetail.java
+++ exam/src/main/java/com/ustudy/exam/model/answersheet/EgsDetail.java
@@ -17,11 +17,42 @@ public class EgsDetail implements Serializable {
 	 */
 	private static final long serialVersionUID = 6450568638532754622L;
 	
+	/**
+	 * @author jared
+	 * only contains question id and question name information
+	 */
+	class QuesMeta {
+		private long quesid = 0;
+		private String quesname = null;
+		
+		public QuesMeta(long quesid, String quesname) {
+			super();
+			this.quesid = quesid;
+			this.quesname = quesname;
+		}
+
+		public long getQuesid() {
+			return quesid;
+		}
+
+		public void setQuesid(long quesid) {
+			this.quesid = quesid;
+		}
+
+		public String getQuesname() {
+			return quesname;
+		}
+
+		public void setQuesname(String quesname) {
+			this.quesname = quesname;
+		}
+
+	}
 	private String subName = null;
 	private long egsid = 0;
-	List<String> quesName = null;
-	List<String> bestL = null;
-	List<String> faqL = null;
+	List<QuesMeta> quesL = null;
+	List<QuesMeta> bestL = null;
+	List<QuesMeta> faqL = null;
 	
 	public EgsDetail() {
 		super();
@@ -30,7 +61,8 @@ public class EgsDetail implements Serializable {
 
 	/**
 	 * @param detail with format as below
-	 * 语文-28@5-6-8-9-13-14-15-16-20-21-22@-@-
+	 * 语文-28@254-5-255-6-252-8-257-9-258-13-250-14-261-15-260-16-259-20-251-21-253-22@-@-
+	 * 物理-31@295-14-299-15-297-16-294-17-298-18@294-17-298-18@-
 	 */
 	public EgsDetail(String detail) {
 		String [] paras = detail.split("@");
@@ -39,11 +71,11 @@ public class EgsDetail implements Serializable {
 			if (datas != null && datas.length == 2) {
 				this.subName = datas[0];
 				this.egsid = Long.valueOf(datas[1]);
-				this.quesName = new ArrayList<String>();
-				assembleList(paras[1], this.quesName);
-				this.bestL = new ArrayList<String>();
+				this.quesL = new ArrayList<QuesMeta>();
+				assembleList(paras[1], this.quesL);
+				this.bestL = new ArrayList<QuesMeta>();
 				assembleList(paras[2], this.bestL);
-				this.faqL = new ArrayList<String>();
+				this.faqL = new ArrayList<QuesMeta>();
 				assembleList(paras[3], this.faqL);
 			}
 		}
@@ -65,44 +97,48 @@ public class EgsDetail implements Serializable {
 		this.subName = subName;
 	}
 
-	public List<String> getQuesName() {
-		return quesName;
+	public List<QuesMeta> getQuesL() {
+		return quesL;
 	}
 
-	public void setQuesName(List<String> quesName) {
-		this.quesName = quesName;
+	public void setQuesL(List<QuesMeta> quesL) {
+		this.quesL = quesL;
 	}
 
-	public List<String> getBestL() {
+	public List<QuesMeta> getBestL() {
 		return bestL;
 	}
 
-	public void setBestL(List<String> bestL) {
+	public void setBestL(List<QuesMeta> bestL) {
 		this.bestL = bestL;
 	}
 
-	public List<String> getFaqL() {
+	public List<QuesMeta> getFaqL() {
 		return faqL;
 	}
 
-	public void setFaqL(List<String> faqL) {
+	public void setFaqL(List<QuesMeta> faqL) {
 		this.faqL = faqL;
 	}
 
 	/**
-	 * @param details format as below
+	 * @param details format as below, question id and question no pair
+	 * 177-13:16-170-17-175-18-176-19-174-20-171-21-173-22
 	 */
-	private void assembleList(String para, List<String> pList) {
+	private void assembleList(String para, List<QuesMeta> pList) {
 		String [] items = para.split("-");
-		if (items != null && items.length > 0) {
-			for (String item: items) {
-				pList.add(item);
+		if (items != null && items.length > 0 && (items.length %2  == 0)) {
+			for (int i = 0; i < items.length; i += 2) {
+				// assemble question name here 
+				items[i+1].replaceAll(":", "-");
+				pList.add(new QuesMeta(Long.valueOf(items[i]), items[i+1]));
 			}
 		}
 	}
+	
 	@Override
 	public String toString() {
-		return "EgsDetail [subName=" + subName + ", egsid=" + egsid + ", quesName=" + quesName + ", bestL=" + bestL
+		return "EgsDetail [subName=" + subName + ", egsid=" + egsid + ", quesL=" + quesL + ", bestL=" + bestL
 				+ ", faqL=" + faqL + "]";
 	}
 
